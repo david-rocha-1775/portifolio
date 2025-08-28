2b623326ae9f1425d46448349c03e4da9cbe9417
David Rocha <davrocha1006@gmail.com>
2025-08-28 08:20:36 -0300

    Add files via upload

--- projetos-academicos/linguagem C/atividades faculdade/ex004.c
+++ projetos-academicos/linguagem C/atividades faculdade/ex004.c
@@ -8,6 +8,6 @@ int main(){
     scanf("%f",&n2);
     printf("digite a terceira nota: ");
     scanf("%f",&n3);
-    media = (n1*1) * (n2*2) *(n3*3);
+    media = ((n1*1) + (n2*2) + (n3*3))/3;
     printf("media com peso 1,2,3 respectivamente =%.2f",media);
 }
