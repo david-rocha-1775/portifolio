7890b74b968aec8442caa774c36a18d601de012f
David Rocha <davrocha1006@gmail.com>
2025-09-02 09:09:11 -0300

    Update ex015.c

--- projetos-academicos/linguagem C/atividades faculdade/ex015.c
+++ projetos-academicos/linguagem C/atividades faculdade/ex015.c
@@ -1,12 +1,13 @@
 /*Implemente uma função chamada powAPC que calcule XY. Sua função deve receber os valores de X e Y, ambos inteiros, e imprimir o valor de Xy em ponto flutuante.*/
 #include <stdio.h>
 #include <math.h>
-float powAPC(int x, int y){
-    return pow(x,y);
+void powAPC(int x, int y){
+    float resultado = pow(x,y);
+    printf("%.2f\n", resultado);
 }
 int main(){
     int x, y;
     scanf("%d %d", &x, &y);
-    printf("%.2f\n",powAPC(x,y));
+    powAPC(x,y);
     return 0;
 }
