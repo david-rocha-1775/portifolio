92450a2287569571a2ea6d5e4555bf7c4704d3bf
david rocha <davrocha1006@gmail.com>
2025-11-01 13:36:46 -0300

    organizando portifolio

--- projetos-academicos/dev web/projeto site/php/conexão.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-    $conexao = new PDO ("mysql: host=localhost; dbname=petr1","root","");
-    $conexao -> setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
-}catch(PDOexpetion $e){
-    echo "Erro de conexão:" . $e-> getMessage();
-    die();
-}
-?>
